f0e8b17537210d321150cecf6f49fd3f0b90c74f
SALVA LUPETA <salvakanzeka1@gmail.com>
2024-01-08 16:55:05 +0200

    2.Repartition des 3 bloc Lundi 8 jan 16:55

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,18 @@ function App() {
 
   return (
     <>
-      <div className='principale_container'>
-        <div className='left_container'></div>
-        <div className='mindle_container'>
+      <div className='principal_container bg-slate-400 m-8 '>
+        <div className='left_container bg-neutral-500'>
+          <img src="src/images/Vector.svg" alt="logo twitter" />
+        </div>
+        <div className='mindle_container bg-orange-400'>
           <header> <div>HOME</div><div>✨</div> </header>
           <section>
           </section>
         </div>
-        <div className='right_container' ></div>
+        <div className='right_container bg-lime-400' >
+          <img src="src/images/Search-Icon.svg" alt="icon recherche" />
+        </div>
       </div>
     </>
   )
