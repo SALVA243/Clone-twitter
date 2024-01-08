06df0a58ee953c04b07ac316c6713a846a401835
SALVA LUPETA <salvakanzeka1@gmail.com>
2024-01-08 17:59:52 +0200

    3. formatage du premier bloc Lundi 8 jan 18:00

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ function App() {
 
   return (
     <>
-      <div className='principal_container bg-slate-400 m-8 '>
-        <div className='left_container bg-neutral-500'>
+      <div className='principal_container bg-slate-400 m-8 flex flex-row w-3/4 m-auto'>
+        <div className='left_container bg-neutral-500 basis-1/4'>
           <img src="src/images/Vector.svg" alt="logo twitter" />
         </div>
-        <div className='mindle_container bg-orange-400'>
+        <div className='mindle_container bg-orange-400 basis-1/2'>
           <header> <div>HOME</div><div>✨</div> </header>
           <section>
           </section>
         </div>
-        <div className='right_container bg-lime-400' >
-          <img src="src/images/Search-Icon.svg" alt="icon recherche" />
+        <div className='right_container bg-lime-400 basis-1/4' >
+          <div className='rounded-full'>
+            <img src="src/images/Search-Icon.svg" alt="icon recherche" />
+            <p>Search twitter</p>
+          </div>
         </div>
       </div>
     </>
